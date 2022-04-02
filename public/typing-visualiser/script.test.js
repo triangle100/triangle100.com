ec59a7aa1910e934ffd1613f6262634641e99d5d
Zane <zaneshaw.home@gmail.com>
2022-04-02 12:45:05 +1100

    Revert "Revert "Initial wipe""
    This reverts commit 4111274b9fd4eb7cbdc0fc3b6f25c3672086f465.

--- public/typing-visualiser/script.test.js
+++
@@ -1,5 +0,0 @@
-const script = require('../typing-visualiser/script');
-
-test('gets input and splits it into seperate characters', () => {
-    expect(script.SetupChars("test")).toBe(["t", "e", "s", "t"]);
-});
