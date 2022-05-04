83513b7a94f0a642b6c3b5e0ee026e4f139ee602
Zane Shaw <zaneshaw.home@gmail.com>
2022-05-04 14:44:52 +1000

    Change main app content

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import logo from './logo.png';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>h1</h1>
+				<h2>h2</h2>
+				<h3>h3</h3>
+				<h4>h4</h4>
+				<h5>h5</h5>
+				<h6>h6</h6>
+				<p>Text</p>
+				<code>Code</code>
+				<a href="">Link</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
