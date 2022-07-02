40d7cb27f5fe2d0e19e9b1fd0434f275d04a09ba
Zane <zaneshaw.home@gmail.com>
2022-07-02 21:35:38 +1000

    Create SEO.svelte

---
+++ src/lib/SEO.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let title = "triangle100";
+    export let description = "triangle100";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    <html lang="en" />
+</svelte:head>
