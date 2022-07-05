687ef75e4fb2fa7daf9d1cba7bb35a4f594c5a06
Zane <zaneshaw.home@gmail.com>
2022-07-06 00:56:57 +1000

    Add 'tech stack modal' to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,16 @@
 <script>
     import SEO from "$lib/components/SEO.svelte";
+    import Modal from "$lib/components/Modal/Modal.svelte";
+
+    let modalOpen = false;
+
+    function openModal() {
+        modalOpen = true;
+    }
+
+    function closeModal() {
+        modalOpen = false;
+    }
 </script>
 
 <SEO title="triangle100" description="" />
@@ -7,7 +18,11 @@
 <div id="container">
     <h1>triangle100</h1>
     <p>Website is currently undergoing construction</p>
+
+    <p>Meanwhile, you can check out this project's <span class="link" on:click={openModal}>tech stack</span></p>
 </div>
+
+<Modal modalOpen={modalOpen} on:closeModal={closeModal} />
 
 <style>
     h1 {
@@ -20,5 +35,11 @@
         position: fixed;
         left: 50%;
         transform: translateX(-50%);
+    }
+
+    .link {
+        border-bottom: 2px dotted; 
+        color: rgb(85, 130, 255);
+        cursor: pointer;
     }
 </style>
