23294d1b34abdab6b714ecf5f795ed7b60edb651
Zane <zaneshaw.home@gmail.com>
2022-07-05 16:32:10 +1000

    Apply 'Montserrat' to layout

---
+++ src/lib/components/Font.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <link
+        href="https://fonts.googleapis.com/css?family=Montserrat"
+        rel="stylesheet"
+    />
+</svelte:head>
+
+<style>
+    :global(*) {
+        font-family: Montserrat;
+    }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,1 +1,6 @@
+<script>
+    import Font from "$lib/components/Font.svelte";
+</script>
+
+<Font />
 <slot />
