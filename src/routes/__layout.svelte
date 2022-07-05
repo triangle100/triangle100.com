b34726e244e469ee6b34a53c9e62e22d56d320f0
Zane <zaneshaw.home@gmail.com>
2022-07-05 18:52:05 +1000

    Handle all unhandled rejections

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,13 @@
 <script>
+    import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
     import Font from "$lib/components/Font.svelte";
+
+    onMount(() => {
+		window.onunhandledrejection = (e) => {
+			goto("/");
+		}
+	})
 </script>
 
 <Font />
