45a2c091c980cd15d6d1cb34fd79782cb9cc4b34
Zane <zaneshaw.home@gmail.com>
2022-07-06 00:57:36 +1000

    Create 'ToolIcon' component for tech stack tools

---
+++ src/lib/components/ToolIcon.svelte
@@ -0,0 +1,45 @@
+<script>
+    import Image from "$lib/components/Image.svelte";
+
+    export let label;
+    export let src;
+    export let target;
+
+    function openLink() {
+        window.open(target, "_blank");
+    }
+</script>
+
+<div id="icon" on:click={openLink} title={label}>
+    <Image {src} alt={label} />
+    <span class="aligner"></span>
+</div>
+
+<style lang="scss">
+    #icon {
+        display: inline-block;
+        margin: 0px 5px;
+        padding: 10px;
+        white-space: nowrap;
+
+        border-radius: 10px;
+        width: 45px;
+        height: 45px;
+        background-color: rgba(0, 0, 0, 0.05);
+
+        cursor: pointer;
+        white-space: nowrap;
+        text-align: center;
+        
+        .aligner {
+            display: inline-block;
+            height: 100%;
+            vertical-align: middle;
+        }
+
+        &:hover {
+            filter: brightness(90%);
+            background-color: rgba(0, 0, 0, 0.1);
+        }
+    }
+</style>
