dc078f56bb10dd9a12e3d9a6ea0e0abb7ebbec73
Zane <zaneshaw.home@gmail.com>
2022-07-05 16:31:34 +1000

    Create error route

---
+++ src/routes/__error.svelte
@@ -0,0 +1,42 @@
+<script context="module">
+    /** @type {import('@sveltejs/kit').Load} */
+    export function load({ error, status }) {
+        return {
+            props: { error, status },
+        };
+    }
+</script>
+
+<script>
+    /** @type {number} */
+    export let status;
+
+    /** @type {Error & {frame?: string} & {loc?: object}} */
+    export let error;
+</script>
+
+<div id="container">
+    <h1>{status}</h1>
+    <h2>{error.message}</h2>
+
+    {#if error.frame}
+        <pre>{error.frame}</pre>
+    {/if}
+    {#if error.stack}
+        <pre>{error.stack}</pre>
+    {/if}
+</div>
+
+<style>
+    h1, h2 {
+        text-align: center;
+        margin: 0px;
+    }
+
+    #container {
+        margin-top: 20px;
+        position: fixed;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+</style>
