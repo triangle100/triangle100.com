4d9f26be6e4cd5ea0741f14dc9274cbd3a46c54b
Zane <zaneshaw.home@gmail.com>
2022-07-05 20:20:31 +1000

    Personalise index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import SEO from "$lib/components/SEO.svelte";
+</script>
+
+<SEO title="triangle100" description="" />
+
+<div id="container">
+    <h1>triangle100</h1>
+    <p>Website is currently undergoing construction</p>
+</div>
+
+<style>
+    h1 {
+        margin: 0px;
+    }
+
+    #container {
+        text-align: center;
+        margin-top: 20px;
+        position: fixed;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+</style>
