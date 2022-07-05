3b00be1e8098aef84a3079b6adcec6d1b5ac33d1
Zane <zaneshaw.home@gmail.com>
2022-07-06 08:33:38 +1000

    Fix static building

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: sveltePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		})
+	},
+	prerender: {
+		default: true
 	},
 	tralingSlash: "always"
 };
