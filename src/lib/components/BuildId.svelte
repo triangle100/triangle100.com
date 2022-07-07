e5c48e34d66e39cbd0a72a8d39b30e7a360aa6e8
Zane <zaneshaw.home@gmail.com>
2022-07-08 03:33:08 +1000

    Add universal build number

--- src/lib/components/BuildId.svelte
+++ src/lib/components/BuildId.svelte
@@ -1,7 +1,6 @@
 <script>
-    let buildNumber = 0;
-    let buildTime = new Date().getTime();
-    let buildDate = new Date().toISOString();
+    let buildNumber = "";
+    let buildTime = 0;
 
     const owner = "triangle100";
     const repo = "triangle100-com";
@@ -18,7 +17,7 @@
         .then((response) => response.json())
         .then((data) => {
             const runId = data.workflow_runs[0].id;
-            buildNumber = data.workflow_runs[0].run_number;
+            buildNumber = `1.0.${data.workflow_runs[0].run_number}.`;
 
             return fetch(`${url}/runs/${runId}/jobs`, reqInit);
         })
@@ -31,9 +30,20 @@
             return obj.name === "Run npm ci && npm run build";
         });
         const createdAt = new Date(step[0].completed_at);
+        
         buildTime = createdAt.getTime();
+        buildNumber += date2Build(createdAt);
     });
 
+    function date2Build(date) {
+        const midnight = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 0, 0, 0);
+        const year = date.getUTCFullYear().toString().slice(-2);
+        const month = ("0" + (date.getUTCMonth() + 1)).slice(-2);
+        const day = ("0" + date.getUTCDate()).slice(-2);
+        const minute2 = ("000" + Math.floor((date.getTime() - midnight.getTime()) / 1000 / 60 / 2)).slice(-3);
+
+        return `${year}${month}${day}${minute2}`;
+    }
 </script>
 
 <div>
