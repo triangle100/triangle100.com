2119ee1fcc996ed421600ee600830df3596767bf
Zane Shaw <zaneshaw.home@gmail.com>
2022-07-21 15:04:22 +1000

    Add template content to blog and project page

---
+++ src/routes/blog.svelte
@@ -0,0 +1,7 @@
+<h1>Blog</h1>
+
+<style lang="scss">
+    h1 {
+        margin: 0px;
+    }
+</style>
---
+++ src/routes/projects.svelte
@@ -0,0 +1,7 @@
+<h1>Projects</h1>
+
+<style lang="scss">
+    h1 {
+        margin: 0px;
+    }
+</style>
