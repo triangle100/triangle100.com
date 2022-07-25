6d6b39149c60c2814e1c142cbbd86b21e22f5ef5
Zane Shaw <admin@triangle100.com>
2022-07-25 22:11:58 +1000

    Remove `.then` for sign in

--- src/lib/components/AdminLogin.svelte
+++ src/lib/components/AdminLogin.svelte
@@ -23,23 +23,20 @@
             const email = emailInput.value;
             const password = passwordInput.value;
 
-            signIn(email, password)
-                .then((res) => {
-                })
-                .catch((error) => {
-                    const pretty = error.code.split("/")[1].replace(/-/g, " ");
-                    submitting = false;
+            signIn(email, password).catch((error) => {
+                const pretty = error.code.split("/")[1].replace(/-/g, " ");
+                submitting = false;
 
-                    if (error.code == "auth/wrong-password") {
-                        passwordInput.setCustomValidity(pretty);
-                        passwordInput.reportValidity();
-                    } else {
-                        emailInput.setCustomValidity(pretty);
-                        emailInput.reportValidity();
-                    }
-                    
-                    console.error("Error signing in!", error.code);
-                });
+                if (error.code == "auth/wrong-password") {
+                    passwordInput.setCustomValidity(pretty);
+                    passwordInput.reportValidity();
+                } else {
+                    emailInput.setCustomValidity(pretty);
+                    emailInput.reportValidity();
+                }
+
+                console.error("Error signing in!", error.code);
+            });
         } else {
             submitting = false;
         }
