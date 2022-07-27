6bfede5c2b9901f2ab5ac82e64ed54c3c25b9e49
Zane Shaw <zaneshaw.home@gmail.com>
2022-07-27 14:49:50 +1000

    Add `404` screen
    + Contact button

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -19,33 +19,41 @@
 
 <SEO title={status.toString()} />
 
-<h1>{status}</h1>
-<h2>{error.message}</h2>
-<div id="error-text">
-    {#if error.frame}
-        <pre>{error.frame}</pre>
-    {/if}
-    {#if error.stack}
-        <pre>{error.stack}</pre>
-    {/if}
-</div>
+{#if status == 404}
+    <h1>Page not found!</h1>
+    <h3>Sorry, I couldn't find the page you requested.</h3>
+    <p>
+        Check your spelling and try again. If you believe something is supposed
+        to be here, you can <a href="/contact" target="_blank">contact</a> me.
+    </p>
+{:else}
+    <h1>{status}</h1>
+    <h2>{error.message}</h2>
+    <div id="error-text">
+        {#if error.frame}
+            <pre>{error.frame}</pre>
+        {/if}
+        {#if error.stack}
+            <pre>{error.stack}</pre>
+        {/if}
+    </div>
+    <p>
+        If you believe this error is my fault, you can <a
+            href="/contact"
+            target="_blank">contact</a
+        > me.
+    </p>
+{/if}
+
 <p>Return to <a href="/">Home</a></p>
 
 <style lang="scss">
-    h1 {
-        margin-bottom: 0px;
-    }
-
-    h2 {
-        margin-top: 0px;
-    }
-    
     #error-text {
         text-align: left;
 
         margin: 0px auto;
         max-width: 80%;
         max-height: 50vh;
-        overflow-y: scroll;
+        overflow-y: auto;
     }
 </style>
