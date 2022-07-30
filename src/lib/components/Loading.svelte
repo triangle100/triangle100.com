8b1e875b492045e5fca7a613c51d5a65fe12b56e
Zane Shaw <admin@triangle100.com>
2022-07-30 17:29:58 +1000

    Fix `Loading` alignment and size

--- src/lib/components/Loading.svelte
+++ src/lib/components/Loading.svelte
@@ -1,14 +1,8 @@
 <script>
     import loadingGif from "$lib/assets/loading.gif";
 
-    export let size = "70";
     export let id = null;
 </script>
 
-<img {id} class="loading" src={loadingGif} alt="Loading..." style="--size: {size+"px"}" />
+<img {id} class="w-16 mx-auto" src={loadingGif} alt="Loading..." />
 
-<style lang="scss">
-    .loading {
-        width: var(--size);
-    }
-</style>
