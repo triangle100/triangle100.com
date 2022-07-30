7d618fa55d173c0b6cc1ab6c55c510463f3ef86e
Zane Shaw <admin@triangle100.com>
2022-07-30 15:57:45 +1000

    Add "new tab" icon

--- src/lib/components/TechIcon.svelte
+++ src/lib/components/TechIcon.svelte
@@ -13,9 +13,14 @@
 
 <Tooltip content={label}>
     <Button
-        class="!p-3 w-16 h-16 !bg-gray-100 hover:!bg-gray-200 hover:scale-110 transition ease-in-out duration-300 !ring-0"
+        class="!p-3 w-16 h-16 !bg-gray-100 hover:!bg-gray-200 hover:scale-110 transition ease-in-out duration-300 !ring-0 relative"
         on:click={openLink}
     >
+        <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg=="
+            alt="New tab"
+            class="absolute top-1 right-1"
+        />
         <RichImage {src} alt={label} />
     </Button>
 </Tooltip>
