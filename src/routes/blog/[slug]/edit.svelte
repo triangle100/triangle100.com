2cf3b0d014253e58ff6ad3334893d3d30510a99d
Zane Shaw <admin@triangle100.com>
2022-08-02 17:33:06 +1000

    Add auth check

--- src/routes/blog/[slug]/edit.svelte
+++ src/routes/blog/[slug]/edit.svelte
@@ -1,6 +1,7 @@
 <script>
     import { page } from "$app/stores";
     import { goto } from "$app/navigation";
+    import { user } from "$lib/stores/userStore";
     import { onMount } from "svelte";
     import { getPost, editPost } from "$lib/services/firebase/db";
     import SEO from "$lib/components/SEO.svelte";
@@ -14,8 +15,13 @@
     let _title;
     let content;
     let el = {};
+    $: loggedIn = !!$user;
 
     onMount(async () => {
+        if (!loading && !loggedIn) {
+            goto("/admin");
+        }
+
         post = await getPost($page.params.slug);
 
         loading = false;
@@ -47,37 +53,39 @@
 
 <SEO title="Editor - {title}" desc="Blog edtior" />
 
-<h1>{loading ? "Loading..." : `Editing ${_title}`}</h1>
-<div class="text-left">
-    <div class="[&>*]:w-full">
-        <input
-            id="title"
-            placeholder="Title"
-            class="mb-1 !p-2"
-            disabled={loading}
-            bind:this={el.title}
-            on:input={updatePreview}
-        />
-        <textarea
-            id="content"
-            placeholder="Content"
-            class="!px-2 !py-2 resize-y min-h-[24em]"
-            disabled={loading}
-            bind:this={el.content}
-            on:input={updatePreview}
-        />
+{#if loggedIn}
+    <h1>{loading ? "Loading..." : `Editing ${_title}`}</h1>
+    <div class="text-left">
+        <div class="[&>*]:w-full">
+            <input
+                id="title"
+                placeholder="Title"
+                class="mb-1 !p-2"
+                disabled={loading}
+                bind:this={el.title}
+                on:input={updatePreview}
+            />
+            <textarea
+                id="content"
+                placeholder="Content"
+                class="!px-2 !py-2 resize-y min-h-[24em]"
+                disabled={loading}
+                bind:this={el.content}
+                on:input={updatePreview}
+            />
+        </div>
     </div>
-</div>
-<div class="text-left mb-2">
-    <h2>Preview</h2>
-    <div class="border border-black rounded p-5">
-        <Blog {post} {loading} bind:title bind:content />
+    <div class="text-left mb-2">
+        <h2>Preview</h2>
+        <div class="border border-black rounded p-5">
+            <Blog {post} {loading} bind:title bind:content />
+        </div>
     </div>
-</div>
-<ActionButton
-    on:click={handleSubmit}
-    action={posting}
-    actionText="Posting..."
-    buttonClass="float-right"
-    >Post
-</ActionButton>
+    <ActionButton
+        on:click={handleSubmit}
+        action={posting}
+        actionText="Posting..."
+        buttonClass="float-right"
+        >Post
+    </ActionButton>
+{/if}
