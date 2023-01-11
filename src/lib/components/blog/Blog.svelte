f666474fe27ce0a2eed19d322bb3ec7e29a19916
Zane Shaw <admin@triangle100.com>
2023-01-11 11:53:51 +1100

    Update blog page

--- src/lib/components/blog/Blog.svelte
+++ src/lib/components/blog/Blog.svelte
@@ -2,6 +2,7 @@
 	import Loading from "$lib/components/Loading.svelte";
 	import NoBlog from "$lib/components/blog/NoBlog.svelte";
 	import Markdown from "$lib/components/Markdown.svelte";
+	import { DateTime } from "luxon";
 
 	export let loading = true;
 	export let post;
@@ -19,11 +20,19 @@
 {#if loading}
 	<Loading />
 {:else if post.data}
-	<div class="text-left">
-		<div class="mt-4 mb-8 [&>*]:inline-block">
-			<h1 class="my-0">{title}</h1>
-			<span class="ml-1">by <b>{post.data.author}</b></span>
+	<div class="mt-4 mb-8 flex flex-col">
+		<h1 class="my-0 font-bold">{title}</h1>
+		<div class="flex justify-center gap-2 text-bluegray-400">
+			<span>
+				{DateTime.fromSeconds(post.data.createdAt.seconds).toFormat(
+					"DDD"
+				)}
+			</span>
+			<span>•</span>
+			<span>{post.data.author}</span>
 		</div>
+	</div>
+	<div class="text-left w-[600px] mx-auto">
 		<Markdown raw={content} />
 	</div>
 {:else}
