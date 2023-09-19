d4fb60b434f2f7e6f6e3f0a7bf2c3ca567e1adee
Zane Shaw <admin@triangle100.com>
2023-09-19 21:57:47 +1000

    Add SquidCraft route

---
+++ src/routes/(minecraft)/squidcraft/+page.svelte
@@ -0,0 +1,5 @@
+<div class="w-2/3 mx-auto text-center py-6">
+	<h2><b>SquidCraft M</b> is here!</h2>
+	<p>Sign up to the whitelist <a href="https://forms.gle/TgFfubVcGbYoT3Aj6" target="_blank" rel="noreferrer">here</a>.</p>
+	<p>Current modlist can be found <a href="https://docs.google.com/spreadsheets/d/1ecrhwtUkpbVpWSsOO2ekazh9unhMCRQhsrXVORF8A8I/edit?usp=sharing" target="_blank" rel="noreferrer">here</a>.</p>
+</div>
